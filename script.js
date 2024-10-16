052589402998081066261bccee2af25351364df2
Gideon.L <gidonlenge96@gmail.com>
2024-10-16 15:27:30 +0200

    done

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 const wrapper = document.querySelector(".wrapper"),
 qrinput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
-qrimag = wrapper.querySelector(".qr-code img");
+qrimag = wrapper.querySelector(".qr-code img"),
+qrcolor = wrapper.querySelector(".color input");
 
 generateBtn.addEventListener("click", () =>{
+    let color = qrcolor.value;
     let qrvalue = qrinput.value;
     if(!qrvalue) return;
     generateBtn.innerText = "Generator QR Code..."
-    qrimag.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrvalue}`;
+    qrimag.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrvalue}${color}`;
     qrimag.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generator QR Code"
@@ -20,3 +22,4 @@ qrinput.addEventListener("keyup", () => {
     }
 
 });
+
