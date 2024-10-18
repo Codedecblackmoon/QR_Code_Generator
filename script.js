b641651be1f2c500baf0e5448b37ade7f1fa659c
Codacblackmoon <gideonlenge96@gmail.com>
2024-10-18 19:32:53 +0200

    working

--- script.js
+++ script.js
@@ -1,15 +1,13 @@
 const wrapper = document.querySelector(".wrapper"),
 qrinput = wrapper.querySelector(".form input"),
 generateBtn = wrapper.querySelector(".form button"),
-qrimag = wrapper.querySelector(".qr-code img"),
-qrcolor = wrapper.querySelector(".color input");
+qrimag = wrapper.querySelector(".qr-code img");
 
 generateBtn.addEventListener("click", () =>{
-    let color = qrcolor.value;
     let qrvalue = qrinput.value;
     if(!qrvalue) return;
     generateBtn.innerText = "Generator QR Code..."
-    qrimag.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrvalue}${color}`;
+    qrimag.src=`https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=${qrvalue}`;
     qrimag.addEventListener("load", () => {
         wrapper.classList.add("active");
         generateBtn.innerText = "Generator QR Code"
